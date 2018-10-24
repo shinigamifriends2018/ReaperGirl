896566930c41748e9fb659fdc5a0b00e829d1876
LAPTOP-6TLBLQUB\human <shinigamifriends2018@gmail.com>
2018-10-24 15:36:26 +0900

    新規作成

---
+++ Unity/ReaperAndAGirlsMoment/Assets/Scripts/ShinigamiController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinigamiController : MonoBehaviour {
+
+    [SerializeField]
+    float m_moveSpeed = 4f;
+    [SerializeField]
+    float m_jumpPower = 5f;
+    Vector2 scale;
+    Rigidbody2D rb;
+    bool m_toConnectHands;
+
+    //  [SerializeField]
+    //   Transform m_syoujo;
+    Vector2 m_shinigamisPos;
+
+    // Use this for initialization
+    void Start () {
+        scale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_shinigamisPos = gameObject.transform.position;
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(new Vector2(m_moveSpeed * h * Time.deltaTime, 0f));
+        if(h > 0)
+        {
+            if (scale.x < 0)
+            {
+                scale.x *= -1f;
+            }
+        } else if(h < 0)
+        {
+            if (scale.x > 0)
+            {
+                scale.x *= -1f;
+            }
+        }
+            
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * m_jumpPower, ForceMode2D.Impulse);
+        }
+
+ /*       if (Input.GetButtonDown("ConnectHands"))
+        {
+            if(m_toConnectHands == true)
+            {
+                gameObject.transform.parent = m_syoujo;
+            }
+        }       
+*/
+        transform.localScale = scale;
+	}
+  /*  private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "syoujo")
+        {
+            m_toConnectHands = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "syoujo")
+        {
+            m_toConnectHands = false;
+        }
+    }
+    */
+    public Vector2 Posinvestigate
+    {
+        get
+        {
+            return m_shinigamisPos;
+        }
+    }
+}
---
+++ Unity/ReaperAndAGirlsMoment/Assets/Scripts/SyoujoController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SyoujoController : MonoBehaviour
+{
+    [SerializeField]
+    float m_moveSpeed = 3f;
+    int m_aFeelingOfBelieve = 3;
+    bool m_followMode;
+    public ShinigamiController shinigami;
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(m_aFeelingOfBelieve >= 3)
+        {
+            m_followMode = true;
+        }
+        else
+        {
+            m_followMode = false;
+        }
+
+        if (m_followMode == true)
+        {
+            if (shinigami.Posinvestigate.x > transform.position.x)
+            {
+                transform.Translate(new Vector2(m_moveSpeed * Time.deltaTime, 0f));
+            }
+            else if (shinigami.Posinvestigate.x < transform.position.x)
+            {
+                transform.Translate(new Vector2(-m_moveSpeed * Time.deltaTime, 0f));
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(m_followMode == true)
+        {
+
+        }
+    }
+}
