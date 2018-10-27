fc36e419cf02cecfa071eb2a892842235895e7e2
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-10-27 18:05:35 +0900

    チュートリアル表示プログラムを修正

--- Unity/ReaperAndAGirlsMoment/Assets/Scripts/NearController.cs
+++ Unity/ReaperAndAGirlsMoment/Assets/Scripts/NearController.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class NearController : MonoBehaviour{
 
-    private int m_enemyHP = 2;
+    public int m_enemyHP = 2;
+    public GameObject m_testmove;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
        
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+	
+	// Update is called once per frame
+	void Update ()
     {
-        if (collision.gameObject.tag == ("Sickle"))
+
+        if (m_enemyHP == 0)
+        {
+            TestMove testMove = m_testmove.GetComponent<TestMove>();
+            testMove.m_returnCheck = true;
+            Destroy(this.gameObject, 0.3f);
+        }
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("sinigami"))
         {
             --m_enemyHP;
-            if (m_enemyHP == 0)
-            {
-                Destroy(this.gameObject,0.3f);
-            }
         }
     }
 }
--- Unity/ReaperAndAGirlsMoment/Assets/Scripts/TestMove.cs
+++ Unity/ReaperAndAGirlsMoment/Assets/Scripts/TestMove.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestMove : MonoBehaviour {
+    [SerializeField]
+    float m_walk = 2f;
 
-    public float m_walk = 2f;
-    bool m_mooveCheck = true;
-	// Use this for initialization
-	void Start () {
+    public int m_textCounter = 0;
 
+    public GameObject[] m_ImageDis;
 
+    public GameObject[] m_enemyDisPlay;
+
+    private bool m_imageCheck = true;
+
+    private bool m_pointCheck = true;
+
+    private bool m_enemyTextCheck = false;
+
+    public string[] m_text;
+
+    public Text m_textUI;
+
+    public bool m_returnCheck = false;
+
+    bool m_enemyTextCounter = true;
+
+    public NearController nearController;
+    // Use this for initialization
+    void Start () {
+        m_textUI.text = m_text[m_textCounter];
     }
 
     // Update is called once per frame
-    void Update () {
+     void Update () {
 
         Vector2 scale = transform.localScale;
         Vector2 pos = transform.position;
@@ -28,6 +49,134 @@ public class TestMove : MonoBehaviour {
             transform.Translate(new Vector2(-m_walk * Time.deltaTime, 0f));
 
         }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (m_textCounter == 2)
+            {
+                TextDisPlay();
+                m_ImageDis[0].SetActive(m_imageCheck);    
+            }
+            else if (m_textCounter == 3)
+            {
+                Time.timeScale = 1;
+                TextDisPlay();
+                m_imageCheck = false;
+                m_ImageDis[0].SetActive(m_imageCheck);
+                m_ImageDis[3].SetActive(m_imageCheck);
+
+            }
+            else if(m_textCounter == 5 || m_textCounter == 6 || m_textCounter == 9 )
+            {
+                TextDisPlay();
+            }
+            else if (m_textCounter == 7)
+            {
+                TextDisPlay();
+                m_imageCheck = false;
+                m_ImageDis[3].SetActive(m_imageCheck);
+                Time.timeScale = 1;
+                if(m_enemyTextCheck == true)
+                {
+                    m_ImageDis[2].SetActive(m_imageCheck);
+                    Time.timeScale = 1;
+                }
+            }
+            else if (m_textCounter == 10)
+            {
+                m_imageCheck = false;
+                m_ImageDis[2].SetActive(m_imageCheck);
+                Time.timeScale = 1;
+            }
+        }
+
+        if (m_returnCheck == true)
+        {
+            if (m_enemyTextCounter == true)
+            {
+                if (m_textCounter == 4)
+                {
+                    m_imageCheck = true;
+                    m_ImageDis[3].SetActive(m_imageCheck);
+                    Invoke("TimeDestroy", 0.3f);
+                    TextDisPlay();
+                    m_enemyTextCounter = false;
+                }
+                else if (m_textCounter > 5)
+                {
+                    m_imageCheck = true;
+                    m_ImageDis[2].SetActive(m_imageCheck);
+                    m_ImageDis[3].SetActive(m_imageCheck);
+                    Invoke("TimeDestroy", 0.3f);
+                    m_textCounter = 4;
+                    TextDisPlay();
+                    m_enemyTextCheck = true;
+                    m_enemyTextCounter = false;
+                }
+            }
+            m_returnCheck = false;
+        }
+
         transform.localScale = scale;
+    }
+
+    public void TextDisPlay()
+    {
+        ++m_textCounter;
+        m_textUI.text = m_text[m_textCounter];
+    }
+
+    void TimeDestroy()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tutorial1")
+        {
+            if (m_textCounter == 0)
+            {
+                TextDisPlay();
+            }
+        }
+        if (collision.gameObject.tag == "Tutorial2")
+        {
+            if (m_textCounter == 1)
+            {
+                TextDisPlay();
+                m_enemyDisPlay[0].SetActive(m_imageCheck);
+                m_enemyDisPlay[1].SetActive(m_imageCheck);
+                m_enemyDisPlay[2].SetActive(m_imageCheck);
+                m_ImageDis[3].SetActive(m_imageCheck);
+                Time.timeScale = 0;
+            }
+        }
+        if (collision.gameObject.tag == "Tutorial3")
+        {
+            if (m_pointCheck == true)
+            {
+                if (m_textCounter == 8)
+                {
+                    TextDisPlay();
+                    m_imageCheck = true;
+                    m_enemyDisPlay[3].SetActive(m_imageCheck);
+                    m_enemyDisPlay[4].SetActive(m_imageCheck);
+                    m_ImageDis[3].SetActive(m_imageCheck);
+                    Time.timeScale = 0;
+                    m_pointCheck = false; 
+                }
+                else if (m_textCounter < 8)
+                {
+                    m_textCounter = 8;
+                    TextDisPlay();
+                    m_imageCheck = true;
+                    m_enemyDisPlay[3].SetActive(m_imageCheck);
+                    m_enemyDisPlay[4].SetActive(m_imageCheck);
+                    m_ImageDis[3].SetActive(m_imageCheck);
+                    Time.timeScale = 0;
+                    m_pointCheck = false;
+                }
+            }
+        }
     }
 }
